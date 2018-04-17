740af00133774bb68cff643e3d03d3a0d7655e7f
adonis <adonisdoda@gmail.com>
2018-04-17 14:20:56 -0300

    Aplicação git slider

--- app/src/main/java/com/example/adonis/stackice/MainActivity.java
+++ app/src/main/java/com/example/adonis/stackice/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.adonis.stackice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
