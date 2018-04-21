8086fe03638904f6078c1d9a88737695a099caa2
Doda <adonisdoda@gmail.com>
2018-04-21 04:28:11 -0300

    tudo

---
+++ app/src/main/java/com/example/adonis/stackice/FontManager.java
@@ -0,0 +1,19 @@
+package com.example.adonis.stackice;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FontManager {
+
+    public static final String ROOT = "fonts/",
+            FONTAWESOME = ROOT + "fontawesome-webfont.ttf";
+
+    public static Typeface getTypeface(Context context, String font) {
+        return Typeface.createFromAsset(context.getAssets(), font);
+    }
+
+
+}
--- app/src/main/java/com/example/adonis/stackice/MainActivity.java
+++ app/src/main/java/com/example/adonis/stackice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.adonis.stackice;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
@@ -9,27 +11,28 @@ import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
-        addSlide(new FragmentSlide.Builder()
-               .fragment(R.layout.primeiro_slider)
-                .build());
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
 
         addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
+                .backgroundDark(android.R.color.white)
+                .fragment(R.layout.primeiro_slider)
+                .build() );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
+                .backgroundDark(android.R.color.white)
                 .fragment(R.layout.segundo_slider)
-                .build());
-
-        addSlide(new FragmentSlide.Builder()
-                .fragment(R.layout.segundo_slider)
-                .build());
-
-
-
-
-
+                .canGoForward(false)
+                .build() );
 
 
 
