49eaaac0d3a651dcca7f75a2dbf3a7bbeddf5410
Doda <adonisdoda@gmail.com>
2018-04-24 03:09:48 -0300

    tudo

--- app/src/main/java/com/example/adonis/stackice/SlideActivity.java
+++ app/src/main/java/com/example/adonis/stackice/SlideActivity.java
@@ -1,6 +1,10 @@
 package com.example.adonis.stackice;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,14 +13,15 @@ import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
-public class MainActivity extends IntroActivity {
+public class SlideActivity extends IntroActivity {
 
-
+     private static final String ARQUIVO_PREERENCIA = "ARQUIVOPREFERENCIA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+
 
         setButtonBackVisible(false);
         setButtonNextVisible(false);
@@ -31,12 +36,22 @@ public class MainActivity extends IntroActivity {
                 .background(android.R.color.holo_blue_light)
                 .backgroundDark(android.R.color.white)
                 .fragment(R.layout.segundo_slider)
-                .canGoForward(false)
                 .build() );
 
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_orange_dark)
+                .backgroundDark(android.R.color.white)
+                .fragment(R.layout.teceiro_slider)
+                .build());
 
-
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_orange_dark)
+                .backgroundDark(android.R.color.white)
+                .fragment(R.layout.quarto_slider)
+                .canGoForward(false)
+                .build());
 
 
     }
+
 }
